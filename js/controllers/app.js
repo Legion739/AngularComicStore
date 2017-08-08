892ca0420e06c23300f595279e91747871eef70b
Legion739 <legion739@gmail.com>
2017-08-08 09:58:22 +0200

    Add Basic Navigation
    add basic navigation between suppliers, plae order and about

--- js/controllers/app.js
+++ js/controllers/app.js
@@ -1,14 +1,20 @@
 var app = angular.module('myApp', []);
 app.controller('StoreController',[ '$scope', '$http', function($scope, $http) {
 
-    $scope.testVar = "It's Alive !!";
+    //initial values
     $scope.suppliers = [];
+    $scope.activePage = "placeOrder";
 
     // get suppliers
     $http.get('http://frontendshowcase.azurewebsites.net/api/Suppliers')
     .then(function(suppliers) {
         $scope.suppliers = suppliers.data;
     });
+
+    // Set active page
+    $scope.goToPage = function(page){
+      $scope.activePage = page;
+    };
 
 
 }]);
