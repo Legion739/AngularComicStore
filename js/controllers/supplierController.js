234123576cddd5a4eeabbf72cc128e3af19d0810
Legion739 <legion739@gmail.com>
2017-08-15 14:33:06 +0200

    Clean up modal stuuf a bit

--- js/controllers/supplierController.js
+++ js/controllers/supplierController.js
@@ -98,64 +98,30 @@ app.controller('SupplierController', [ '$scope', 'showcaseService', function($sc
 
 
 
-app.controller('SupplierDeleteCtrl', function ($uibModal, $log, $document) {
+app.controller('SupplierCreateCtrl', function ($uibModal, $log, $document) {
   var $ctrl = this;
-  $ctrl.items = ['item1', 'item2', 'item3'];  
-
-  $ctrl.animationsEnabled = true;
 
   $ctrl.open = function (size, parentSelector) {
-    var parentElem = parentSelector ?
-      angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
     var modalInstance = $uibModal.open({
-      animation: $ctrl.animationsEnabled,
+      animation: true,
       ariaLabelledBy: 'modal-title',
       ariaDescribedBy: 'modal-body',
       templateUrl: './html/modalPlaceOrder.html',
       controller: 'ModalInstanceCtrl',
       controllerAs: '$ctrl',
       size: size,
-      appendTo: parentElem,
       resolve: {
-        items: function () {
-          return $ctrl.items;
-        }
+        /*items: function () {
+          return null;
+        }*/
       }
     });
 
-    modalInstance.result.then(function (selectedItem) {
-      $ctrl.selected = selectedItem;
+    modalInstance.result.then(function () {
+      //modal dismissed
     }, function () {
       $log.info('Modal dismissed at: ' + new Date());
     });
-  };
-
-  $ctrl.openMultipleModals = function () {
-    $uibModal.open({
-      animation: $ctrl.animationsEnabled,
-      ariaLabelledBy: 'modal-title-bottom',
-      ariaDescribedBy: 'modal-body-bottom',
-      templateUrl: 'stackedModal.html',
-      size: 'sm',
-      controller: function($scope) {
-        $scope.name = 'bottom';
-      }
-    });
-
-    $uibModal.open({
-      animation: $ctrl.animationsEnabled,
-      ariaLabelledBy: 'modal-title-top',
-      ariaDescribedBy: 'modal-body-top',
-      templateUrl: 'stackedModal.html',
-      size: 'sm',
-      controller: function($scope) {
-        $scope.name = 'top';
-      }
-    });
-  };
-
-  $ctrl.toggleAnimation = function () {
-    $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
   };
 
 });
@@ -163,21 +129,17 @@ app.controller('SupplierDeleteCtrl', function ($uibModal, $log, $document) {
 // Please note that $uibModalInstance represents a modal window (instance) dependency.
 // It is not the same as the $uibModal service used above.
 
-app.controller('ModalInstanceCtrl', function ($uibModalInstance, items) {
+app.controller('ModalInstanceCtrl',['$uibModalInstance', function ($uibModalInstance) {
   var $ctrl = this;
-  $ctrl.items = items;
-  $ctrl.selected = {
-    item: $ctrl.items[0]
-  };
 
   $ctrl.ok = function () {
-    $uibModalInstance.close($ctrl.selected.item);
+    $uibModalInstance.close();
   };
 
   $ctrl.cancel = function () {
     $uibModalInstance.dismiss('cancel');
   };
-});
+}]);
 
 app.filter('startFrom', function() {
     return function(input, start) {
