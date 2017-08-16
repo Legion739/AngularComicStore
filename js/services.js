24f6dbee3ab6f63b44c5263b9e66687a16df88fb
Legion739 <legion739@gmail.com>
2017-08-16 09:23:16 +0200

    Change api url to https

--- js/services.js
+++ js/services.js
@@ -1,7 +1,7 @@
 var app = angular.module('myApp.services', []);
 
 app.factory('showcaseService', function($resource) {
-  return $resource('http://frontendshowcase.azurewebsites.net/api/:action/:id', {}, {
+  return $resource('https://frontendshowcase.azurewebsites.net/api/:action/:id', {}, {
       update: {
         method: 'PUT' // this method issues a PUT request
       },
